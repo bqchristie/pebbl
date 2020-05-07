2df0fdfe56d4a1220b1ccb8c5f435fa98915b97f
Bruce <bqchristie@gmail.com>
2020-05-07 15:11:23 -0400

    setting up https

--- api/server.js
+++ api/server.js
@@ -1,14 +1,19 @@
-var config = require('./config/config')
-express = require('express'),
+const config = require('./config/config'),
+    express = require('express'),
     app = express(),
     cors = require('cors'),
-    port = process.env.PORT || 80,
+    fs = require('fs'),
+    http = require('http'),
+    https = require('https'),
     mongoose = require('mongoose'),
-    require('./models/user'), //created model loading here
-    require('./models/list'), //created model loading here
     bodyParser = require('body-parser'),
     routes = require('./routes/api');
 
+require('dotenv').config();
+require('./models/user'); //created model loading here
+require('./models/list'); //created model loading here
+
+console.log(process.env.ENVIRONMENT);
 
 app.use(bodyParser.urlencoded({extended: false}))
 app.use(bodyParser.json());
@@ -51,6 +56,38 @@ app.use(function (req, res) {
 });
 
 
-app.listen(port);
+const httpPort = process.env.HTTP_PORT || 3000;        // set our port
+const httpsPort = process.env.HTTPS_PORT;        // set our port
 
-console.log('todo list RESTful API server started on: ' + port);
+
+// app.listen(port);
+
+if (httpsPort) {
+    // Certificate
+    const key = fs.readFileSync('/etc/letsencrypt/live/api.fuudlist.com/privkey.pem', 'utf8');
+    const certificate = fs.readFileSync('/etc/letsencrypt/live/api.fuudlist.com/cert.pem', 'utf8');
+    const cert = fs.readFileSync('/etc/letsencrypt/live/api.fuudlist.com/fullchain.pem', 'utf8');
+    const ca = fs.readFileSync('/etc/letsencrypt/live/api.fuudlist.com/chain.pem', 'utf8');
+
+    const credentials = {
+        cert: cert,
+        key: key
+    };
+
+    const httpsServer = https.createServer(credentials, app);
+
+    httpsServer.listen(httpsPort, () => {
+        console.log('HTTPS Server running on port ' + httpsPort);
+    });
+} else {
+    const httpServer = http.createServer();
+    httpServer.listen(httpPort, () => {
+        console.log('todo list RESTful API server started on: ' + httpPort);
+    })
+
+}
+
+
+// NOTE:
+// check out this to configure SSL
+// https://github.com/bqchristie/dbtools/blob/master/server.js
